4ee799255e41e33ec21bfc449856fdb259d4c480
Ioakeim Hadjimpalasis <ioakeim.h@gmail.com>
2023-05-22 18:23:53 +0300

    Update data_search.py

--- DataGlitch/data_search.py
+++ DataGlitch/data_search.py
@@ -13,18 +13,28 @@ def column_search(df, col, score_cutoff=80):
     column_to_match = col
     options = df.columns
 
-    result = process.extract(query=column_to_match, choices=options,
+    matches = process.extract(query=column_to_match, choices=options,
                              limit=None, score_cutoff=score_cutoff)
-    print(result)
+    
+    for match in matches:
+        print("Match: ", match[0])
+        print("Similarity Score: ", match[1])
+        print()
 
 
-def value_search(col, value):
+def value_search(col, value, score_cutoff=80):
     if not isinstance(col, pd.Series):
         raise TypeError("Input data should be a Pandas Series")
     if not isinstance(value, str):
         raise TypeError("value argument should be of type str")
     
     col = col.astype(str)
-    result = process.extractOne(value, col, scorer=fuzz.ratio)
-    print(result)
+    matches = process.extract(value, col, scorer=fuzz.ratio, limit=None)
+    
+    # Filter matches above threshold
+    filtered_matches = [match for match in matches if match[1] >= score_cutoff]
+    for match in filtered_matches:
+        print("Match: ", match[0])
+        print("Similarity Score: ", match[1])
+        print()
 
